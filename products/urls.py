9d8caceaf92b8f0e15245c4954bdcd96c6a65f5a
Srinivasboggula202 <168057407+Srinivasboggula202@users.noreply.github.com>
2025-08-22 22:02:39 +0530

    37297

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
-    path()
+    path('menu_items/',MenuItemListView.as_view(), name="menu_items"),
 ]
