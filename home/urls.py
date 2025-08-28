e86246634033f377fd747f865ce6882b797d4435
Srinivasboggula202 <168057407+Srinivasboggula202@users.noreply.github.com>
2025-08-29 01:14:35 +0530

    37301

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('home/',views.home),
+    path('',views.home),
     path('about/',views.about),
     path('contact/',views.contact_us),
 ]
