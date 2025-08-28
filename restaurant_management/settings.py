c5786d1a88ae0538a78466287b62203ff93a3302
Srinivasboggula202 <168057407+Srinivasboggula202@users.noreply.github.com>
2025-08-29 01:15:54 +0530

    37301

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'restaurant_management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,9 +123,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIR = [
-    BASE_DIR/'static'
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
